1607b3415e77293e7d0d1e1303b55b24995d8a72
Erick Joel Malan <erickjpk1234@gmail.com>
2024-02-06 22:00:22 -0500

    AHI XD

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 //Aqui debes trabajar para exportar toda la página
 //Aqui puedes importar otros componentes que hagas
-
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
